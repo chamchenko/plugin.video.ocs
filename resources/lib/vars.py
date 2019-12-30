a88006d46b7c94e28aa582870cfbaa6bdf423277
chamchenko <chamchenko@gmail.com>
2019-12-30 15:22:38 +0100

    few changes:   resources/lib/vars.py 	added add/remove favorites to ocs profile: resources/lib/favorites.py

--- resources/lib/vars.py
+++ resources/lib/vars.py
@@ -35,10 +35,12 @@ SHOWS_API       = API_BURL + API_TargetWeb + 'rubriques/series?limit=22'
 SHOW_API        = API_BURL + API_TargetWeb + 'details/serie/%s'
 DOCS_API        = API_BURL + API_TargetWeb + 'rubriques/documentaires?limit=22'
 MAGAZINES_API   = API_BURL + API_TargetWeb + 'rubriques/magazines?limit=22'
+FAVORITES_API   = API_BURL + API_TargetWeb + 'rubriques/favoris?limit=22'
 HOUSEHOLD_API   = API_BURL + API_TargetWeb + 'households'
 PLAYER_API      = API_BURL + '/core/v1/play/webplayer?cid=%s'
 SEARCH_API      = API_BURL + API_TargetWeb + 'contents?search=%s&limit=22'
 SEARCH_KIDS_API = API_BURL + API_TargetWeb + 'contents?filter=jeunesse&search=%s&limit=22'
+ADD_FAV_API     = API_BURL + API_TargetWeb + 'users/bookmarks/%s'
 KIDS_SUB_MENU   = [(LANGUAGE(40008), API_BURL + API_TargetWeb + "rubriques/jeunesse?filter=seriejeunesse&limit=22", 2),
                    (LANGUAGE(40003), API_BURL + API_TargetWeb + "rubriques/jeunesse?filter=film&limit=22", 5),
                    (LANGUAGE(40007) ,"",            11)]
@@ -49,5 +51,5 @@ MAIN_MENU       = [(LANGUAGE(40001), "",            1),
                    (LANGUAGE(40004), "",            6),
                    (LANGUAGE(40005), DOCS_API,      8),
                    (LANGUAGE(40006), MAGAZINES_API, 8),
-                   (LANGUAGE(40007) ,"",            7),
-                   (LANGUAGE(40009) ,"",           12)]
+                   (LANGUAGE(40009) ,FAVORITES_API, 8),
+                   (LANGUAGE(40007) ,"",            7)]
