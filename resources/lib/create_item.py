50bc4383eee7a4af046ebfa94f6fc6e232813f81
chamchenko <chamchenko@gmail.com>
2019-12-30 15:08:40 +0100

    added favorites context menu:   resources/lib/create_item.py

--- resources/lib/create_item.py
+++ resources/lib/create_item.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import sys, xbmcgui, xbmcplugin, urllib
+import sys, xbmc, xbmcgui, xbmcplugin, urllib
 from vars import *
 from tools import log
-def addLink(name, u, mode, infoList=False, infoArt=False, total=0, infoCast=False):
+from os import sep as osSeparator
+def addLink(name, u, mode, infoList=False, infoArt=False, total=0, favID=None, infoCast=None):
     name = name.encode("utf-8")
     log('addLink, name = ' + name)
     liz=xbmcgui.ListItem(name)
@@ -11,14 +12,17 @@ def addLink(name, u, mode, infoList=False, infoArt=False, total=0, infoCast=Fals
     else: liz.setInfo(type="Video", infoLabels=infoList)
     if infoArt == False: liz.setArt({'thumb':ICON,'fanart':FANART})
     else: liz.setArt(infoArt)
-    if infoCast != False: liz.setCast(infoCast)
+    if infoCast: liz.setCast(infoCast)
+    if favID:
+        script = ADDON_PATH+osSeparator+'resources'+osSeparator+'lib'+osSeparator+'favorites.py'
+        actionadd       = 'xbmc.RunScript('+script+','+ favID+',PUT)'
+        actiondelete    = 'xbmc.RunScript('+script+','+ favID+',DELETE)'
+        liz.addContextMenuItems([('Add to OCS Favorites', actionadd),('Remove from OCS Favorites', actiondelete)])
     u=sys.argv[0]+"?url="+urllib.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
-    #is_folder = False
-    #return (u, liz, is_folder)
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,totalItems=total)
 
 
-def addDir(name, u, mode=None, infoArt=False, infoList=False, total=0, infoCast=False):
+def addDir(name, u, mode=None, infoArt=False, infoList=False, total=0, favID=None, infoCast=False):
     name = name.encode("utf-8")
     log('addDir, name = ' + name)
     liz=xbmcgui.ListItem(name)
@@ -28,5 +32,10 @@ def addDir(name, u, mode=None, infoArt=False, infoList=False, total=0, infoCast=
     if infoArt == False: liz.setArt({'thumb':ICON,'fanart':FANART})
     else: liz.setArt(infoArt)
     if infoCast != False: liz.setCast(infoCast)
+    if favID:
+        script = ADDON_PATH+osSeparator+'resources'+osSeparator+'lib'+osSeparator+'favorites.py'
+        actionadd       = 'xbmc.RunScript('+script+','+ favID+',PUT)'
+        actiondelete    = 'xbmc.RunScript('+script+','+ favID+',DELETE)'
+        liz.addContextMenuItems([(LANGUAGE(40015), actionadd),(LANGUAGE(40016), actiondelete)])
     u=sys.argv[0]+"?url="+urllib.quote_plus(u)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
     xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True,totalItems=total)
